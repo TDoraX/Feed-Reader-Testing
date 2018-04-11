d90a4572498a36b379198844dffadbcee63b9b92
TDoraX <zhangze612@163.com>
2018-04-12 01:02:02 +0800

    Test code modified

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -11,6 +11,16 @@ $(function() {
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
      */
     describe('RSS Feeds', function() {
+        function sameDetection(key) {
+            for (let i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].hasOwnProperty(key)).toBe(true);
+                expect(allFeeds[i][key]).toBeTruthy();
+                if (key === 'url') {
+                    var regularExpressionUrl = /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/g;
+                    expect(allFeeds[i].url).toMatch(regularExpressionUrl);
+                }
+            }
+        }
         /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
          * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
          * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
@@ -26,28 +36,14 @@ $(function() {
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
         it('has URL property and not empty', function() {
-            for (var i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].hasOwnProperty('url')).toBe(true);
-                // expect(allFeeds[i].url).toBeDefined();
-                // expect(allFeeds[i].url).not.toBeNull();
-                // expect(allFeeds[i].url).not.toBe('');
-                // 不知下面的代码是否可以实现和上述代码同样的效果
-                expect(allFeeds[i].url).toBeTruthy();
-            }
+            sameDetection('url');
         });
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
         it('has name property and not empty', function() {
-            for (var i = 0; i < allFeeds.length; i++) {
-                expect(allFeeds[i].hasOwnProperty('name')).toBe(true);
-                // expect(allFeeds[i].name).toBeDefined();
-                // expect(allFeeds[i].name).not.toBeNull();
-                // expect(allFeeds[i].name).not.toBe('');
-                // 不知下面的代码是否可以实现和上述代码同样的效果
-                expect(allFeeds[i].name).toBeTruthy();
-            }
+            sameDetection('name');
         });
     });
 
@@ -78,7 +74,7 @@ $(function() {
     describe('Initial Entries', function() {
         beforeEach(function(done) {
             loadFeed(1, done);
-        }, 10000)
+        }, 30000)
         /* TODO:
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -88,23 +84,29 @@ $(function() {
          */
         it('should be called and works well', function() {
             expect($('.feed .entry').length).toBeGreaterThan(0);
-        })
+        });
     });
-    
+
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
     describe('New Feed Selection', function() {
-        var conDefault = $('.feed').html();
-        
+        var getContent2,
+            getContent3;
         beforeEach(function(done) {
-            loadFeed(2, done);
-        }, 10000);
+            loadFeed(2, function() {
+                getContent2 = $('.feed').html();
+                loadFeed(3, function() {
+                    getContent3 = $('.feed').html();
+                    done();
+                })
+            });
+        }, 30000);
 
         /* TODO:
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
         it('should be changed when new feed loaded', function() {
-            expect(conDefault).not.toBe($('.feed').html());
-        })
+            expect(getContent2).not.toBe(getContent3);
+        });
     });
 }());
